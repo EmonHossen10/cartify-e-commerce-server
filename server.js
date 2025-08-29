edb29d2aea3f2adcbdd767362b239149bec50a67
EmonHossen10 <emonhossen1000@gmail.com>
2025-08-30 01:02:40 +0600

    mongoose bug fixed

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: [
       "Content-Type",
@@ -33,4 +33,7 @@ app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+app.get("/", (req, res) => {
+  res.send("Server is running 🚀");
 });
